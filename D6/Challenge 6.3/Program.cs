181e593622194e33e61f19d9de17b9aa70589e5a
SalahEldin <salaheldinm.1999@gmail.com>
2025-06-13 21:01:01 +0300

    update

---
+++ D6/Challenge 6.3/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Challenge_6._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
