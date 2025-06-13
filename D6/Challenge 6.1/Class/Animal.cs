5228ffac3e1e663a66700ec8f77bcba2a0afd218
SalahEldin <salaheldinm.1999@gmail.com>
2025-06-13 18:19:18 +0300

    update Challenge 1

---
+++ D6/Challenge 6.1/Class/Animal.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace Challenge_6._1
+{
+    public class Animal
+    {
+        public string? Name { get; set; }
+        public int? Age { get; set; }
+        public virtual string AnimalSound()
+        {
+           return "The animal makes a sound";
+        }
+    }
+    public class Pig : Animal
+    {
+        public string? Size { get; set; }
+        public override string AnimalSound()
+        {
+            return "The pig says: wee wee";
+        }
+    }
+    public class Dog : Animal
+    {
+        public string? Size { get; set; }
+        public override string AnimalSound()
+        {
+            return "The dog says: bow wow";
+        }
+    }
+}
--- D6/Challenge 6.1/Program.cs
+++ D6/Challenge 6.1/Program.cs
@@ -5,6 +5,16 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            Animal myAnimal = new Animal();     // Create a Animal object
+            Animal myPig = new Pig();           // Create a Pig object
+            Animal myDog = new Dog();           // Create a Dog object
+
+            Console.WriteLine(myAnimal.AnimalSound()); // The animal makes a sound
+            Console.WriteLine(myPig.AnimalSound());    // The pig says: wee wee
+            Console.WriteLine(myDog.AnimalSound());    // The dog says: bow wow
+
+
         }
     }
 }
