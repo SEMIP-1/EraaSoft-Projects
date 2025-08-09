4d760facf540d85e558946e806e8738dcdea372d
SEMIP <SEMIP@DESKTOP-1>
2025-08-09 21:48:52 +0300

    update

---
+++ Assignment 9/PinoHospital/ViewModels/DoctorVM.cs
@@ -0,0 +1,8 @@
+﻿
+    public class DoctorVM
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Specialization { get; set; } = string.Empty;
+        public string? Img { get; set; }
+    }
