fb9a07e10956495436bfbb3cc560fa3caccba483
SEMIP <SEMIP@DESKTOP-1>
2025-08-31 07:42:16 +0300

    update

--- Assignment 10/MAXCINA/Models/Cinemas.cs
+++ Assignment 10/MAXCINA/Models/Cinemas.cs
@@ -2,5 +2,9 @@
 {
     public class Cinemas
     {
+        public string Name { get; set;}
+        public string Description { get; set; }
+        public string CinemaLogo { get; set; }
+        public string Address { get; set; }
     }
 }
